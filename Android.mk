126f9dfcd8e57cef7affde9a0fbe83f097925380
extreme729 <shinema3743@gmail.com>
2025-06-02 16:59:46 +0530

    first commit

---
+++ Android.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2025 Rissu Projects
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),m32)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
+
---
+++ AndroidProducts.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2025 Rissu Projects
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_m32.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_m32-user \
+    twrp_m32-userdebug \
+    twrp_m32-eng
+eng
